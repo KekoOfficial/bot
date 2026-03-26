3539637c7a5b5937962ef8a1dcb797e8f0497c4e
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-03-26 04:50:56 -0300

    Actualizar downloader.py

--- downloader.py
+++ downloader.py
@@ -1,7 +1,12 @@
 import asyncio
 import re
+import os
+import glob
 
-# 🚀 Ejecutar comando y capturar progreso
+TEMP_PATH = "/data/data/com.termux/files/home/temp/"
+
+os.makedirs(TEMP_PATH, exist_ok=True)
+
 async def run_cmd(cmd, progress_callback):
     process = await asyncio.create_subprocess_shell(
         cmd,
@@ -16,40 +21,48 @@ async def run_cmd(cmd, progress_callback):
 
         text = line.decode("utf-8", errors="ignore")
 
-        # 🔥 detectar %
         match = re.search(r'(\d{1,3}\.\d+)%', text)
         if match:
             await progress_callback(match.group(1))
 
     await process.wait()
 
-
-# 🎬 MP4 FLASH SPEED
+# 🎬 MP4
 async def descargar_mp4(link, progress_callback):
+    output = TEMP_PATH + "%(title)s.%(ext)s"
+
     cmd = f'''
     yt-dlp \
     -N 32 \
     --concurrent-fragments 32 \
     --downloader aria2c \
     --downloader-args "aria2c:-x 32 -s 32 -k 1M" \
-    -f "bestvideo[height<=1080][vcodec*=avc1]+bestaudio[acodec*=mp4a]/best[height<=1080]" \
+    -f "bestvideo[height<=1080]+bestaudio/best" \
     --merge-output-format mp4 \
-    --recode-video mp4 \
     --no-playlist \
     --newline \
-    "{link}"
+    -o "{output}" "{link}"
     '''
+
     await run_cmd(cmd, progress_callback)
 
+    files = glob.glob(TEMP_PATH + "*.mp4")
+    return max(files, key=os.path.getctime)
 
-# 🎵 MP3 FLASH SPEED
+
+# 🎵 MP3
 async def descargar_mp3(link, progress_callback):
+    output = TEMP_PATH + "%(title)s.%(ext)s"
+
     cmd = f'''
     yt-dlp \
     -x --audio-format mp3 \
-    -N 16 \
     --no-playlist \
     --newline \
-    "{link}"
+    -o "{output}" "{link}"
     '''
+
     await run_cmd(cmd, progress_callback)
+
+    files = glob.glob(TEMP_PATH + "*.mp3")
+    return max(files, key=os.path.getctime)
