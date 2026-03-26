e621382d4127269c262d93da955f57aee817f996
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-03-26 04:54:50 -0300

    Actualizar queue_system.py

--- queue_system.py
+++ queue_system.py
@@ -2,8 +2,8 @@ import asyncio
 
 queue = asyncio.Queue()
 
-async def add_to_queue(data):
-    await queue.put(data)
+async def add_to_queue(job):
+    await queue.put(job)
 
 async def worker():
     while True:
@@ -11,5 +11,5 @@ async def worker():
         try:
             await job()
         except Exception as e:
-            print(f"Error: {e}")
+            print("❌ Error en job:", e)
         queue.task_done()
